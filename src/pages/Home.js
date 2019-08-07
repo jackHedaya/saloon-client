3734fab225944eed878ccc7ea54d4eed685e3d06
Jack Hedaya <jackehedaya@gmail.com>
2019-08-07 16:17:35 -0400

    stage 1 complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Avatar from "react-avatar";
 
 import "./styles/Home.scss";
 
@@ -6,6 +7,7 @@ function Home() {
   return (
     <div className="home">
       <CategoryDisplay title="Based on Your Interests" />
+      <CategoryDisplay title="Controversial" />
     </div>
   );
 }
@@ -31,6 +33,14 @@ function DiscussionCard(props) {
   return (
     <div className="card">
       <div className="title">{name}</div>
+      <div className="discussed-by">
+        <Avatar name="Jack Hedaya" round size={40} />
+        <Avatar name="John Cena" round size={40} />
+        <span className="text">
+          <div>Discussion by Jack Hedaya, John Cena, and 19 more</div>
+        </span>
+      </div>
+      <div className="description">A well thought out discussion on the pros and cons of legalizing marijuana.</div>
     </div>
   );
 }
