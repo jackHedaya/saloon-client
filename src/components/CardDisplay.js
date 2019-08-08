5f3117073239659be165b4a7f8ec0c0670f94726
Jack Hedaya <jackehedaya@gmail.com>
2019-08-08 17:34:14 -0400

    moved CardDisplay to Home

---
+++ src/components/CardDisplay.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import Avatar from "react-avatar";
+
+function CardDisplay(props) {
+  const { title } = props;
+
+  return (
+    <div className="card-display">
+      <div className="title">{title}</div>
+      <div className="card-row">
+        <DiscussionCard name="Should Marijuana be Legalized?" />
+        <DiscussionCard name="Should Marijuana be Legalized?" />
+        <DiscussionCard name="Should Marijuana be Legalized?" />
+      </div>
+    </div>
+  );
+}
+
+function DiscussionCard(props) {
+  const { name } = props;
+
+  return (
+    <div className="card">
+      <div className="title">{name}</div>
+      <div className="discussed-by">
+        <Avatar name="Jack Hedaya" round size={40} />
+        <Avatar name="John Cena" round size={40} />
+        <span className="text">
+          <div>Discussion by Jack Hedaya, John Cena, and 19 more</div>
+        </span>
+      </div>
+      <div className="description">A well thought out discussion on the pros and cons of legalizing marijuana.</div>
+    </div>
+  );
+}
+
+export default CardDisplay;
