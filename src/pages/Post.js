5d9f393881dd8aae7f597237e2db2fbd51a37b3a
Jack Hedaya <jackehedaya@gmail.com>
2019-09-11 23:54:10 -0400

    added invite someone

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Avatar from "react-avatar";
+import { FiArrowRightCircle } from "react-icons/fi";
 
 import "react-quill/dist/quill.snow.css";
 import "./styles/Post.scss";
@@ -11,7 +13,8 @@ export default function Post() {
       <div className="collaborators">
         <div>Collaborators</div>
         <CollaboratorSection>Active</CollaboratorSection>
-        <CollaboratorSection>Invited</CollaboratorSection>
+        <CollaboratorSection color="gray">Invited</CollaboratorSection>
+        <Invite />
         <div className="post-button">
           <div>Post</div>
         </div>
@@ -34,11 +37,40 @@ function Editor() {
 }
 
 function CollaboratorSection(props) {
-  const { children } = props;
+  const { color, children } = props;
 
   return (
     <div className="section">
       <div className="title">{children}</div>
+      <div className="users">
+        <Collaborator color={color}>Jack Hedaya</Collaborator>
+        <Collaborator color={color}>Abe Kassin</Collaborator>
+        <Collaborator color={color}>Judah Kishk</Collaborator>
+      </div>
+    </div>
+  );
+}
+
+function Collaborator(props) {
+  const { color = undefined } = props;
+  return (
+    <div className="user">
+      <Avatar name={props.children} src={props.image || null} round size={25} />
+      <span className="name" style={{ color }}>
+        {props.children}
+      </span>
+    </div>
+  );
+}
+
+function Invite(props) {
+  return (
+    <div className="invite">
+      <div className="title">Invite Someone</div>
+      <div className="field">
+        <input className="clear-input" placeholder="username" autoComplete="off" />
+        <FiArrowRightCircle className="submit" />
+      </div>
     </div>
   );
 }
