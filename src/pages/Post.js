d504bf6d99ae82d0a8b2b116750bcbd7dee55e56
Jack Hedaya <jackehedaya@gmail.com>
2019-09-11 22:46:56 -0400

    added editor

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -2,12 +2,18 @@ import React from "react";
 
 import "react-quill/dist/quill.snow.css";
 import "./styles/Post.scss";
+import ReactQuill from "react-quill";
 
 export default function Post() {
   return (
     <div className="post">
       <div className="inner">
         <input className="post-title" placeholder="Title" />
+        <ReactQuill
+          theme="snow"
+          modules={{ toolbar: [["bold", "italic", "underline", "strike"], ["link"]] }}
+          formats={["bold", "italic", "underline", "strike", "link"]}
+        />
       </div>
       <div className="collaborators">
         <div>Collaborators</div>
