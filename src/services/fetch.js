bf0130364c72b5a9d1c4cfcdb9df0b991060aa7b
Jack Hedaya <jackehedaya@gmail.com>
2019-09-12 02:43:11 -0400

    allow for custom headers

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -5,9 +5,9 @@
  */
 export default function newFetch(url, options) {
   return fetch(`https://agile-tor-73556.herokuapp.com/api/v1${url}`, {
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json", ...options.headers },
     ...options,
-    body: JSON.stringify(options.body),
+    body: JSON.stringify(options.body)
   }).then(resp => {
     if (resp.ok) return resp.json();
     throw new Error("Request Error");
