537d69ef48ed881efc45aa500ed4bd87284c4b45
Jack Hedaya <jackehedaya@gmail.com>
2019-09-11 22:17:53 -0400

    skeleton complete

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,5 +1,23 @@
 import React from "react";
 
+import "react-quill/dist/quill.snow.css";
+import "./styles/Post.scss";
+
 export default function Post() {
-  return <div className="post"></div>;
+  return (
+    <div className="post">
+      <div className="inner">
+        <input className="post-title" placeholder="Title" />
+      </div>
+      <div className="collaborators">
+        <div>Collaborators</div>
+        <div className="section">
+          <div className="title">Active</div>
+        </div>
+        <div className="section">
+          <div className="title">Invited</div>
+        </div>
+      </div>
+    </div>
+  );
 }
