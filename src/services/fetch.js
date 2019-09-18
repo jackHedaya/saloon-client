1f50aa9b98a7c0479e9c7bc304a68ca57a0d0860
Jack Hedaya <jackehedaya@gmail.com>
2019-09-18 00:34:49 -0400

    corrected custom headers overwriting content type

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -3,11 +3,11 @@
  * @param {string} url
  * @param {import("http").RequestOptions} options
  */
-export default function newFetch(url, options) {
+export default function newFetch(url, { headers, body, ...options }) {
   return fetch(`https://agile-tor-73556.herokuapp.com/api/v1${url}`, {
-    headers: { "Content-Type": "application/json", ...options.headers },
+    headers: { "Content-Type": "application/json", ...headers },
     ...options,
-    body: JSON.stringify(options.body)
+    body: JSON.stringify(body)
   }).then(resp => {
     if (resp.ok) return resp.json();
     throw resp;
