86eb7ee1bfc9d3c9518c459fba6372975be632cb
Jack Hedaya <jackehedaya@gmail.com>
2019-09-25 21:11:57 -0400

    change url depending on if in prod

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -1,12 +1,14 @@
 import path from "path";
 
+const isProd = () => process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+
 /**
  *
  * @param {string} url
  * @param {import("http").RequestOptions} options
  */
 export default function newFetch(url, { headers, body, ...options }) {
-  const fullUrl = path.join("https://agile-tor-73556.herokuapp.com/api/v1", url);
+  const fullUrl = isProd() ? path.join("https://agile-tor-73556.herokuapp.com/api/v1", url) : url;
 
   return fetch(fullUrl, {
     headers: { "Content-Type": "application/json", ...headers },
