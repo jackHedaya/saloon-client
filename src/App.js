983e2bd8970e2afedb287243c57387290bf8fa95
jackHedaya <jackhedaya@Gladyss-MacBook-Air.local>
2020-02-11 21:13:40 -0500

    added about page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Navigation from './components/Navigation'
 import Persist from './components/Persist'
 
 import Home from './pages/Home'
+import About from './pages/About'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Conversation from './pages/Conversation'
@@ -56,6 +57,7 @@ function App() {
             <Switch>
               <Redirect exact from="/" to="/home" />
               <Route exact path="/home" component={Home} />
+              <Route exact path="/about" component={About} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
               <Route exact path="/conversation/:id" component={Conversation} />
