656fb2c75f31104ec6de26c409903bb0499c088f
jackHedaya <jackhedaya@Gladyss-MacBook-Air.local>
2020-03-06 16:17:46 -0500

    moved column-page to global class

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,8 +7,6 @@ import { useWindowWidth } from '@react-hook/window-size'
 
 import chunk from 'lodash.chunk'
 
-import './styles/Home.scss'
-
 function Home() {
   const feed = useFeed()
   const width = useWindowWidth()
@@ -16,7 +14,7 @@ function Home() {
   const splitFeed = chunk(feed, width < 925 ? 1 : width < 1260 ? 2 : 3)
 
   return (
-    <div className="home">
+    <div className="column-page">
       {splitFeed.map((cards, i) => (
         <CardRow cards={cards} key={`CardRow/${i}`} />
       ))}
