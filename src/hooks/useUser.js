9d65e6bc4f571178559497b2fac82b63dd4a39dc
Jack Hedaya <jackehedaya@gmail.com>
2020-04-19 11:47:43 -0400

    prevent destructure error for optional props

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -4,7 +4,7 @@ import * as userService from '../services/user.service'
 
 import useAuth from './useAuth'
 
-export default function useUser({ reload }) {
+export default function useUser({ reload } = {}) {
   const { token, setToken, isLoggedIn, setIsLoggedIn } = useAuth()
   const [user, setUser] = useState(null)
 
